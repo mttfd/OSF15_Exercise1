3db9b2585039caf24ec036b8181aa77a491955a5
Mingqin Tan <mttfd@mail.missouri.edu>
2015-09-05 20:47:43 -0500

    done command.c

--- Exercise1/command.c
+++ Exercise1/command.c
@@ -8,14 +8,26 @@
 #define MAX_CMD_COUNT 50
 #define MAX_CMD_LEN 25
 
-
 	//TODO FUNCTION COMMENT
+/*
+ * PURPOSE: takes user input(first parameter), and constructs a command
+ * INPUTS:
+ *	input input of user that should be a valid command
+ * RETURN:
+ *  If no errors occurred then true
+ *  else false for an error in the process.
+ *
+ **/
 bool parse_user_input (const char* input, Commands_t** cmd) {
-	
+
 	//TODO ERROR CHECK INCOMING PARAMETERS
+	if(input == NULL) {
+		perror("input command is empty.");
+		return false;
+	}
 
 	char *string = strdup(input);
-	
+
 	*cmd = calloc (1,sizeof(Commands_t));
 	(*cmd)->cmds = calloc(MAX_CMD_COUNT,sizeof(char*));
 
@@ -27,20 +39,39 @@ bool parse_user_input (const char* input, Commands_t** cmd) {
 		if (!(*cmd)->cmds[i]) {
 			perror("Allocation Error\n");
 			return false;
-		}	
+		}
 		strncpy((*cmd)->cmds[i],token, strlen(token) + 1);
 		(*cmd)->num_cmds++;
 		token = strtok(NULL, " \n");
 	}
+
 	free(string);
 	return true;
 }
 
 	//TODO FUNCTION COMMENT
+
+/*
+ * PURPOSE: takes a command and destroys it by freeing allocated memories
+ * INPUTS:
+ *	cmd a command to be destroyed
+ * RETURN:
+ *  no returned value
+ *
+ **/
 void destroy_commands(Commands_t** cmd) {
 
 	//TODO ERROR CHECK INCOMING PARAMETERS
-	
+	if(cmd == NULL || *cmd == NULL) {
+		perror("cmd is null\n");
+		return;
+	}
+
+	if((*cmd)->num_cmds > MAX_CMD_COUNT) {
+		perror("invalid command to be destroyed\n");
+		return;
+	}
+
 	for (int i = 0; i < (*cmd)->num_cmds; ++i) {
 		free((*cmd)->cmds[i]);
 	}
